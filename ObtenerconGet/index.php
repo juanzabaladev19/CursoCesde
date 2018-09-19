79cec79c779ab6a0802a6f316c686f54b0e4db44
Unknown <juanzabaladev@gmail.com>
2018-09-19 18:27:36 -0500

    actividad clase 19/09/2018

---
+++ ObtenerconGet/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <form action="registro.php" method="get">
+            <label>Nombre</label>
+            <input type="text" name="nombre" placeholder="nombre">
+            <br><br>
+            <label>Apellido</label>
+            <input type="text" name="apellido" placeholder="apellido">
+            <br><br>
+            <label>Telefono</label>
+            <input type="text" name="telefono" placeholder="telefono">
+            <input type="submit" value="enviar">
+        </form>
+    </body>
+</html>
---
+++ ObtenerconGet/registro.php
@@ -0,0 +1,12 @@
+<?php
+    //Obtener datos por get
+    $usuario = $_GET;
+    echo($usuario['nombre']);
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+
+
